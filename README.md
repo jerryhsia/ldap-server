93aa0ea7e1be62f9111747c965ea3c3e77ee0b46
Jerry <82269916@qq.com>
2021-06-09 12:30:35 +0800

    Init

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-```
-docker run -d --name ldap-server -p 1389:389 --privileged=true jerry9916/ldap-server:latest
+# ldap-server
 
-samba-tool user create jerry9916 Jerry9916#
+```
+docker run -d --name ldap-server -p 8389:389 --privileged=true jerry9916/ldap-server:latest
+
+samba-tool user create Tom Passw0rd
+```
+
+## accounts
+
+```
+DN：CN=Administrator,CN=Users,DC=ldap,DC=kong,DC=com
+PASSWORD：Passw0rd
+
+DN：CN=Jerry,CN=Users,DC=ldap,DC=kong,DC=com
+PASSWORD：Passw0rd
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,24 +11,22 @@ init() {
         samba-tool ou create "OU=$i"
     done
 
-    for i in $( seq 1 5 )
+    for i in $( seq 1 2 )
     do
        samba-tool group add "test-group-$i"
     done
 
-    samba-tool user create euclid --uid euclid --surname euclid --userou OU=mathematicians damng00dcoffEE
-    samba-tool user create einstein --surname einstein --uid einstein --userou OU=scientists damng00dcoffEE
+    samba-tool user create euclid --uid euclid --surname euclid --userou OU=mathematicians Passw0rd
+    samba-tool user create einstein --surname einstein --uid einstein --userou OU=scientists Passw0rd
 
-    users=(User1 User2 Ophelia Desdemona Katherina Hamlet Othello Petruchio MacBeth Jerry)
+    users=(Jerry Tom)
     for i in "${users[@]}"
     do
-        samba-tool user create $i damng00dcoffEE
+        samba-tool user create $i Passw0rd
     done
 
-    samba-tool group addmembers test-group-1 User1,User2,MacBeth,Jerry
-    samba-tool group addmembers test-group-2 Ophelia,Desdemona,Katherina
-    samba-tool group addmembers test-group-3 Hamlet,Othello,Petruchio,MacBeth,Desdemona
-    samba-tool group addmembers test-group-4 euclid,einstein
+    samba-tool group addmembers test-group-1 Jerry,Tom
+    samba-tool group addmembers test-group-2 euclid,einstein
 }
 
 clean() {
